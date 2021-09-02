7855699a33ad21ebe085bc5057e6fef05f8f036c
KalanaRatnayake <kalana.15@cse.mrt.ac.lk>
2021-09-02 13:38:39 +0530

    modified print function of configparser data

--- checkStatistics.py
+++ checkStatistics.py
@@ -5,23 +5,8 @@ config = configparser.ConfigParser()
 
 config.read(os.path.dirname(os.path.abspath(__file__))+"/../config/statistics.ini")
 
-originalDatasetStat = config["Dataset Statistics"]
-trainDataStat = config["Dataset Training Statistics"]
-testDataStat = config["Dataset Testing Statistics"]
-trainDataStatPostProcess = config["Processed Training Statistics"]
-testDataStatPostProcess = config["Processed Testing Statistics"]
-
-print("\n Dataset Distribution : ")
-print(sorted(originalDatasetStat))
-
-print("\n Training Defect set Distribution : ")
-print(sorted(trainDataStat))
-
-print("\n Testing Defect set Distribution : ")
-print(sorted(testDataStat))
-
-print("\n Processed training set Distribution : ")
-print(sorted(trainDataStatPostProcess))
-
-print("\n Processed testing set Distribution : ")
-print(sorted(testDataStatPostProcess))
+for section_name in config.sections():
+    print (section_name)
+    for name, value in sorted(config.items(section_name)):
+        print ('  %s = %s' % (name, value))
+    print
